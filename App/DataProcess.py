55be3ef289ec94c96dad58286995794ec281f07e
wenzhihaosuot <wenzhihaosuot@163.com>
2019-05-18 13:16:48 +0800

    数据处理

--- App/DataProcess.py
+++ App/DataProcess.py
@@ -2,11 +2,17 @@ import csv
 import jieba
 class DataProcess():
     def process(self):
-        f=open("data/distribute.csv","rt", encoding="utf-8")
+        f=open("data/distribute_collectt.csv","rt", encoding="utf-8")
         lines=f.readlines()
-        print(lines)
         for line in lines:
-            print(line)
+            words=jieba.cut(line)
+            rows = []
+            for word in words:
+                if(len(word)>1):
+                    rows.append(word)
+            out = open('data/distribute_clean.csv', 'a', newline='', encoding="utf-8")
+            csv_writer = csv.writer(out)
+            csv_writer.writerow(rows)
         f.close()
 
 if __name__ == '__main__':
