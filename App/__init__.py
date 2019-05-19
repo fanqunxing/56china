9eb4c07bbea850739be05ee6d943ebfe176481ac
fwx426328 <1053800926@qq.com>
2019-05-19 16:43:13 +0800

    删除无用代码

--- App/__init__.py
+++ App/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 
 from App.web import init_webBlue
-from App.UserApi import init_blue
 from App.ext import init_ext
 from App.settings import envs
 from App.nationApi import init_nationBlue
@@ -10,7 +9,6 @@ from App.nationApi import init_nationBlue
 def create_app():
     app = Flask(__name__)
     app.config.from_object(envs.get('dev'))
-    init_blue(app)
     init_webBlue(app)
     init_ext(app)
     init_nationBlue(app)
